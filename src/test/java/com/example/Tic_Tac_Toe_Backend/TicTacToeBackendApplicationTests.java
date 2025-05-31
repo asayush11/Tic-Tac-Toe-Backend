79f3535940237df6cd056318365f73e8c0cca606
Ayush Sharma <asayush35@gmail.com>
2025-05-31 22:22:38 +0530

    Initial Setup

---
+++ src/test/java/com/example/Tic_Tac_Toe_Backend/TicTacToeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tic_Tac_Toe_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
