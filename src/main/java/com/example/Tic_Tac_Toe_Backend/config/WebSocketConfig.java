f2a49c4f371738fb4b590afe61ea75ec7cd050f2
Ayush Sharma <asayush35@gmail.com>
2025-06-01 12:34:15 +0530

    Removing Polyfills

--- src/main/java/com/example/Tic_Tac_Toe_Backend/config/WebSocketConfig.java
+++ src/main/java/com/example/Tic_Tac_Toe_Backend/config/WebSocketConfig.java
@@ -15,6 +15,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/game").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws/game").setAllowedOriginPatterns("*");
     }
 }
