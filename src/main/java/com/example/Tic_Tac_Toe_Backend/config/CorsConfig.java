dd72b32b3239f32cac35aad02b32f3364eba7b14
Ayush Sharma <asayush35@gmail.com>
2025-06-01 16:19:53 +0530

    CORS Headers

--- src/main/java/com/example/Tic_Tac_Toe_Backend/config/CorsConfig.java
+++ src/main/java/com/example/Tic_Tac_Toe_Backend/config/CorsConfig.java
@@ -10,6 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("*");
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
